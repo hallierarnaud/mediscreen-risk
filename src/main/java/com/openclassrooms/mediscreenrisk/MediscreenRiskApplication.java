88c9ec010d14842645d536c54e0de6eb43d67718
hallier <ahallier@isara.fr>
2022-02-22 15:38:08 +0100

    Initial commit

---
+++ src/main/java/com/openclassrooms/mediscreenrisk/MediscreenRiskApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mediscreenrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenRiskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MediscreenRiskApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/openclassrooms/mediscreenrisk/MediscreenRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mediscreenrisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenRiskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
